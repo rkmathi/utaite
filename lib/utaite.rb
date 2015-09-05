2856bd03b12f37724279991883f0342ea32591fb
Ryuichi KAWAMATA <ryuichi.kawamata@dena.jp>
2015-09-05 17:52:47 +0900

    0.0.1

--- lib/utaite.rb
+++ lib/utaite.rb
@@ -1,5 +1,50 @@
-require "utaite/version"
+require 'utaite/version'
+require 'apache_log/parser'
 
 module Utaite
-  # Your code goes here...
+  class SortedLog
+    attr_reader :logs
+
+    def initialize(lines)
+      parser = ApacheLog::Parser.new('common')
+      sorted_logs = []
+      lines.split("\n").each do |line|
+        sorted_logs << "#{parser.parse(line)[:request][:method]} #{parser.parse(line)[:request][:path]}"
+      end
+
+      @logs = srt(sorted_logs)
+    end
+
+    private
+
+    def srt(logs)
+      hash = {}
+      logs.each do |log|
+        hash[log] ||= 0
+        hash[log] += 1
+      end
+      hash
+    end
+  end
+
+  class Utaite
+    def self.run
+      self.out($stdin.read)
+    end
+
+    def self.out(log_lines)
+      sorted_logs = SortedLog.new(log_lines).logs
+      outp(sorted_logs)
+    end
+
+    private
+
+    def self.outp(paths)
+      puts ' times | path'
+      puts '-------|-----'
+      paths.each do |path|
+        puts "#{printf('%6d', path[1])} | #{path[0]}"
+      end
+    end
+  end
 end
--- utaite.gemspec
+++ utaite.gemspec
@@ -1,4 +1,5 @@
 # coding: utf-8
+
 lib = File.expand_path('../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'utaite/version'
@@ -21,4 +22,5 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency 'bundler', '~> 1.10'
   spec.add_development_dependency 'rake', '~> 10.0'
+  spec.add_development_dependency 'apache_log-parser', '~> 3.1'
 end
